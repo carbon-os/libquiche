09cfcd49cbe75310e7147f0b7485b4b824a5df23
carbon-os <259760037+carbon-os@users.noreply.github.com>
2026-04-13 16:52:51 -0700

    updates

--- ports/quiche/portfile.cmake
+++ ports/quiche/portfile.cmake
@@ -1,10 +1,9 @@
 # v0.1.0
-# 
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO carbon-os/libquiche
   REF "v${VERSION}"
-  SHA512 0
+  SHA512 954546d235e54b0e2038bcfa6b968273346a1ddabb8155ffbb845edb051e63023da4921a1a210f9d1784747f36be3d90424c7ad29caecee933db9d04c09bae33
   HEAD_REF main
 )
 
