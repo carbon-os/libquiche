fe1090d23cc6fb772983590009decf6d9a1496bd
carbon-os <259760037+carbon-os@users.noreply.github.com>
2026-04-13 13:38:35 -0700

    chore: fill in git-tree hashes and quiche SHA512

--- ports/quiche/portfile.cmake
+++ ports/quiche/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO carbon-os/libquiche
   REF "v${VERSION}"
-  SHA512 <fill-in-after-tagging>
+  SHA512 bb742605ae5c1c8a13716433ff05c92720980f6e5d68f177aa48b8cbb9d516439e63eec34d1ade81058105cefc0cfe92ac5118c447346df22e3140cb05190c2c
   HEAD_REF main
 )
 
